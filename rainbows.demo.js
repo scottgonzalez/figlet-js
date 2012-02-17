cc28bf4fec3e732dc9aa452e567d0dc379e5ab39
Nathan Hunzaker <nate.hunzaker@gmail.com>
2012-02-17 18:37:38 -0500

    Cleans directories and adds servers

--- rainbows.demo.js
+++ rainbows.demo.js
@@ -1,10 +1,12 @@
 // Rainbows edition by Nate Hunzaker
 
-var Figlet = require("./figlet-node").Figlet;
-var puts = require("sys").puts;
+var Figlet = require("./figlet-node").Figlet,
+    puts   = require("sys").puts,
+    font   = process.argv[2] || "standard",
+    phrase = process.argv.slice(3).join(" ") || "Figlet JS";
 
 // Set the last argument to 'rainbows' to generate a colorful text treatment
-Figlet.write("Figlet JS", "standard", function(str) {
+Figlet.write(phrase, font, function(str) {
     puts(str);
 }, 'rainbows');
 
